8affab610b454dead7e00cff87fdaff5cccb1cc5
andriuszaz <a.zazeckas@gmail.com>
2023-10-20 20:43:10 +0300

    Added completed todos list to separate from uncompleted todos

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,4 +1,13 @@
-import { Box, Checkbox, Typography } from "@mui/material";
+import {
+  Box,
+  Checkbox,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import Stack from "@mui/material/Stack";
 import Divider from "@mui/material/Divider";
 import { NewTodo } from "./NewTodo";
@@ -6,15 +15,25 @@ import { useState } from "react";
 
 export const Todo = () => {
   const [todos, setTodos] = useState<any[]>([]);
+  const [completedTodos, setCompletedTodos] = useState<any[]>([]);
 
   const handleCheckChange = (id: number) => {
     const updatedTodos = [...todos];
     const checkedTodo = updatedTodos[id];
     updatedTodos.splice(id, 1);
-    updatedTodos.push(checkedTodo);
+    // updatedTodos.push(checkedTodo);
     checkedTodo.done = !checkedTodo.done;
-    setTodos(updatedTodos);
+    if (checkedTodo.done === true) {
+      setCompletedTodos([...completedTodos, checkedTodo]);
+    }
+    if (checkedTodo.done === false) {
+      completedTodos.splice(id, 1);
+    }
+    // console.log(checkedTodo, checkedTodo.done);
+    // setTodos(updatedTodos);
   };
+
+  console.log(completedTodos);
 
   console.log(todos);
   const passDataToParent = (list: any) => {
@@ -24,38 +43,82 @@ export const Todo = () => {
 
   return (
     <Box mt={"20px"} p={"20px"}>
+      <NewTodo passDataToParent={passDataToParent} />
       <Stack
         divider={<Divider orientation="horizontal" flexItem />}
         spacing={2}
       >
-        <NewTodo passDataToParent={passDataToParent} />
         {todos.map((item: any, id: number) => (
-          <Box
+          <List
             key={id}
-            display={"flex"}
-            justifyContent="space-between"
             sx={{
-              color: "black",
-              bgcolor: "white",
-              padding: "10px",
+              width: "100%",
+              bgcolor: "background.paper",
               boxShadow: "5px 5px 5px lightgray",
               borderRadius: "5px",
             }}
           >
-            <Typography
-              variant="body1"
-              p={"10px"}
-              sx={{ textDecoration: item.done ? "line-through" : "none" }}
+            <ListItem
+              // sx={{ textDecoration: item.done ? "line-through" : "none" }}
+              secondaryAction={
+                <IconButton edge="end">
+                  <Typography variant="h6">Delete</Typography>
+                </IconButton>
+              }
             >
-              {item.todo}
-            </Typography>
-            <Checkbox
-              checked={item.done}
-              onChange={() => handleCheckChange(id)}
-            />
-          </Box>
+              <ListItemText primary={item.todo} />
+
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={item.done}
+                  onChange={() => handleCheckChange(id)}
+                />
+              </ListItemIcon>
+            </ListItem>
+          </List>
         ))}
       </Stack>
+      {completedTodos.length > 0 && (
+        <Box>
+          <Stack
+            divider={<Divider orientation="horizontal" flexItem />}
+            spacing={2}
+          >
+            <Typography variant="h6">Completed Todos</Typography>
+            {completedTodos.map((item: any, id: number) => (
+              <List
+                key={id}
+                sx={{
+                  width: "100%",
+                  bgcolor: "background.paper",
+                  boxShadow: "5px 5px 5px lightgray",
+                  borderRadius: "5px",
+                }}
+              >
+                <ListItem
+                  // sx={{ textDecoration: item.done ? "line-through" : "none" }}
+                  secondaryAction={
+                    <IconButton edge="end">
+                      <Typography variant="h6">Delete</Typography>
+                    </IconButton>
+                  }
+                >
+                  <ListItemText primary={item.todo} />
+
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={item.done}
+                      onChange={() => handleCheckChange(id)}
+                    />
+                  </ListItemIcon>
+                </ListItem>
+              </List>
+            ))}
+          </Stack>
+        </Box>
+      )}
     </Box>
   );
 };
